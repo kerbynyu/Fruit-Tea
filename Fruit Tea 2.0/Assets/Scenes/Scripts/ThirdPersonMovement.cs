fba67a36e4d035e816b8cb06932e30b5ae5a48e0
StevoPineapple <new.logintesting@gmail.com>
2022-10-09 20:27:23 -0400

    Isground and textures. I just watch a tounament and the player I liked won. It was exciting even though I don't play that game.

--- Fruit Tea 2.0/Assets/Scenes/Scripts/ThirdPersonMovement.cs
+++ Fruit Tea 2.0/Assets/Scenes/Scripts/ThirdPersonMovement.cs
@@ -9,14 +9,22 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform camera;
     private Vector3 _playerVelocity;
 
-    public float speed = 6f;
+    public float speed;
+    public float speedMax;
+    public float speedAcc;
+    public float speedGroundDacc;
+    public float speedAirDacc;
 
     public float turnSmooth = 0.1f;
     private float _turnSmoothVelocity;
 
     private bool _isGrounded;
-    private float _jumpHeight = 5.0f;
-    private float _gravityValue = -9.81f;
+    private float _jumpForceBase = 2.2f;
+    private float _jumpForceAdd = 2.8f;
+    //private float _jumpHeight = 3.0f;
+    private float _gravityValue = -50f;
+
+    Vector3 moveDire;
 
     private void Start()
     {
@@ -44,14 +52,43 @@ public class ThirdPersonMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, turnSmooth);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;            
-            controller.Move(moveDir * speed * Time.deltaTime);            
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            moveDire = moveDir;
+            if (speed < speedMax)
+                speed += speedAcc;
+            else
+                speed = speedMax;
+            controller.Move(moveDir * speed * Time.deltaTime);
+        }
+        else
+        {
+            if (speed > 0)
+            {
+                if (_isGrounded)
+                    speed -= speedGroundDacc;
+                else
+                    speed -= speedAirDacc;
+            }
+            else
+                speed = 0;
+            controller.Move(moveDire * speed * Time.deltaTime);
         }
 
         if (Input.GetButtonDown("Jump") )
         {
-            Debug.Log("jump!");
-            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);            
+            if (_isGrounded)
+            {
+                Debug.Log("jump!"+ (_jumpForceBase * -_jumpForceBase * _gravityValue));
+                _playerVelocity.y += Mathf.Sqrt(_jumpForceBase * -_jumpForceBase * _gravityValue);
+            }
+        }
+        if (Input.GetButton("Jump"))
+        {
+            if (!_isGrounded)
+            {
+                Debug.Log("jumpControl!"+(_jumpForceAdd * -_jumpForceAdd * _gravityValue));
+                _playerVelocity.y += Mathf.Sqrt(_jumpForceAdd * -_jumpForceAdd * _gravityValue) * Time.deltaTime;
+            }
         }
 
         _playerVelocity.y += _gravityValue * Time.deltaTime;
