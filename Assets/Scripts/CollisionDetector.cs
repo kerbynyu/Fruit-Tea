9d95ec45a71b7f5ba5d967de9af70f57821ee738
May <shanglan.may.li@gmail.com>
2022-10-19 05:02:14 -0400

    fixed collision, added form changes that are assigned to keybinds. enemy material color changes depending  on what form used to hit them.

--- Assets/Scripts/CollisionDetector.cs
+++ Assets/Scripts/CollisionDetector.cs
@@ -7,13 +7,58 @@ public class CollisionDetector : MonoBehaviour
 {
     public WeaponController wc;
     public PlayerManager pm;
+    public BasicEnemy be;
+    
+    Material m_Material;
+
+    //public int currentForm = 1;
+
+    private void Start()
+    {
+        //currentForm = pm
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log("collide");
+        m_Material = other.GetComponent<Renderer>().material;
         if (other.CompareTag("Enemy") && wc.IsAttacking)
         {
             Debug.Log("hit");
             //other.GetComponent<Animator>().SetTrigger("Hit");
+            colorChange(pm.form);
         }
+    }
+    
+    public void colorChange(int form)
+    {
+        if (form == 1)
+        {
+            Debug.Log("ayo1");
+            //hitCounter = 2;
+            m_Material.color = Color.red;
+            StartCoroutine(ResetColor());
+        }
+        else if (form == 2)
+        {
+            Debug.Log("ayo2");
+            //hitCounter = 1;
+            m_Material.color = Color.blue;
+            StartCoroutine(ResetColor());
+        }
+        else
+        {
+            Debug.Log("ayo3");
+            //hitCounter = 3;
+            m_Material.color = Color.green;
+            StartCoroutine(ResetColor());
+        }
+    }
+    
+    IEnumerator ResetColor()
+    {
+        yield return new WaitForSeconds(0.5f);
+        m_Material.color = Color.white;
     }
 }
