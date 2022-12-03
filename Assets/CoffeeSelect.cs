5eaebbe3c565aad2c639299e40306f1177b65a62
Kerby <kerbyneo@gmail.com>
2022-12-03 18:17:31 -0500

    Level Select

---
+++ Assets/CoffeeSelect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeSelect : MonoBehaviour
+{
+
+    public Animator anim; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("Left", false);
+        anim.SetBool("Right", false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left")) {
+            anim.SetBool("Left", true);
+            anim.SetBool("Right", false);
+            print("left arrow key is held down");
+
+
+        } else if ((Input.GetKey("right"))) {
+
+            anim.SetBool("Right", true);
+            anim.SetBool("Left", false);
+            print("right arrow key is held down");
+        }
+    }
+}
