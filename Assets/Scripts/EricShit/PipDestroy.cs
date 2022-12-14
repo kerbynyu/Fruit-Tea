0e57f1bdd650c98e6f2515d88480e92b3c132a50
Eric Knee <ericni007@gmail.com>
2022-12-13 19:12:20 -0500

    PipSystem-Rudimentary
    basic pip shooting (bound to J)

---
+++ Assets/Scripts/EricShit/PipDestroy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipDestroy : MonoBehaviour
+{
+    private float timer;
+    public GameObject hold;
+    public bool canShoot;
+    public Vector3 whereTo;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        canShoot = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer+= Time.deltaTime;
+        if(timer < 0.2f)
+        {
+            gameObject.transform.position = hold.transform.position;
+        }
+
+        if(timer > 0.2f && canShoot == true)
+        {
+            ShootAt(whereTo, speed);
+        }
+
+
+
+        if (timer > 2f) //if we dont hit enemy in 2 seconds
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject); //Destroy itself
+        }
+    }
+
+    public void ShootAt(Vector3 whereTo, float speed)
+    {
+        transform.position += transform.forward * speed; //shoots immediately forward
+        transform.position = Vector3.Lerp(transform.position, whereTo, speed);
+
+    }
+
+    public void CanShoot()
+    {
+        canShoot = true;
+    }
+}
--- Assets/Scripts/EricShit/Raycasting.cs
+++ Assets/Scripts/EricShit/Raycasting.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Raycasting : MonoBehaviour
 {
+    public GameObject[] pipPositions;
+    private float i; //which pip we are on
+    public GameObject firedPip;
+    [SerializeField] float pipSpeed;
+    public Vector3 enemyHit;
+    public Vector3 rigidHit;
+    public Vector3 floorHit;
+
+    private GameObject shot;
+    public bool shooting;
+    private Vector3 nearestEnemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pipPositions = GameObject.FindGameObjectsWithTag("Pip"); //easiest way without having to drag all pips
+        i = 1;
     }
 
     // Update is called once per frame
@@ -15,17 +27,85 @@ public class Raycasting : MonoBehaviour
     {
 
         //Raycast for an enemy
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            //RaycastEnemy(10f);
             //RaycastFloor(100f);
-            RaycastRigid(100f);
+            //RaycastRigid(100f);
+            for(int j = 0; j < pipPositions.Length; j++)
+            {
+                if (pipPositions[j].gameObject.name == ("Pip" + i) && shooting)
+                {
+                    shot = Instantiate(firedPip, pipPositions[j].transform.position, pipPositions[j].transform.rotation);
+                    
+                    RaycastEnemy(100f, transform); //can alter the distance to check. the transform is to help indicate where we draw raycast from
+                    shot.GetComponent<PipDestroy>().hold = pipPositions[j]; //set position to hold ats position to the pip location
+
+                }
+            }
+
+            i++;
+            if(i > 5) //more than the 5 pip positions, reset to shoot from first
+            {
+                i = 1;
+            }
+
+
         }
+
+
+        if (shot != null)
+        {
+            
+                if (enemyHit != Vector3.zero)//We only lerp bullets IF u are raycast from player hits an enemy
+                {
+                    shot.GetComponent<PipDestroy>().whereTo = enemyHit;
+                    shot.GetComponent<PipDestroy>().speed = pipSpeed;
+                    shot.GetComponent<PipDestroy>().CanShoot();
+                }
+                else //else we did not have successful hit
+                {
+                shot.GetComponent<PipDestroy>().whereTo = nearestEnemy;
+                shot.GetComponent<PipDestroy>().speed = pipSpeed;
+                shot.GetComponent<PipDestroy>().CanShoot();
+
+            }
+
+
+            //there is no nearby enemy then we can just deny the shot
+        }
+
+
+
     }
 
-    public void RaycastEnemy(float distanceToCheck)
+
+    private void OnTriggerStay(Collider other) //now we use big sphere collider to check for nearest enemy instead
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        Collider[] enemiesClose = Physics.OverlapSphere(transform.position + new Vector3(0, 0, 7), 60);
+        List<Collider> listNear = new List<Collider>(enemiesClose); //create constant list of nearby Enemies
+
+
+        if (listNear.Contains(other) && other.gameObject.tag == "Enemy") //check if we have any enemies in our close circle
+        {
+            Debug.Log("ENEMI");
+            shooting = true;
+            nearestEnemy = other.gameObject.transform.position;
+
+        } else
+        {
+            nearestEnemy = Vector3.zero;
+            shooting = false;
+        }
+
+        
+    }
+
+
+
+
+    public void RaycastEnemy(float distanceToCheck, Transform whereFrom)
+    {
+        Ray ray = new Ray(whereFrom.position, whereFrom.forward);
         RaycastHit hitData;
         if (Physics.Raycast(ray, out hitData, distanceToCheck))
         {
@@ -33,40 +113,51 @@ public class Raycasting : MonoBehaviour
             if (hitData.transform.gameObject.tag == "Enemy") //if we hit da enemy
             {  //This is the tag for all enemies ("Target")
 
-                Debug.Log(hitData.transform.position); //position of whatever we hit
-
+                //Debug.Log(hitData.transform.position); //position of whatever we hit
+                enemyHit = hitData.transform.position;
+            } else
+            {
+                enemyHit = Vector3.zero;
             }
 
         }
     }
 
-    public void RaycastFloor(float distanceToCheck)
+    public void RaycastFloor(float distanceToCheck, Transform whereFrom)
     {
         //raycasts to floor/downward
-        Ray ray = new Ray(transform.position, -transform.up); //checks negative up for down
+        Ray ray = new Ray(whereFrom.position, -whereFrom.up); //checks negative up for down
         RaycastHit hitData;
         if (Physics.Raycast(ray, out hitData, distanceToCheck))
         {
             if (hitData.transform.gameObject.tag == "Floor") //does what I hit have a rigidbody?
             {
-                Debug.Log(hitData.point); //returns exact world position of ray endpoint when we raycast
-                
+                //Debug.Log(hitData.point); //returns exact world position of ray endpoint when we raycast
+                floorHit = hitData.point;
+            }
+            else
+            {
+                floorHit = Vector3.zero;
             }
 
         }        
     }
 
-    public void RaycastRigid(float distanceToCheck)
+    public void RaycastRigid(float distanceToCheck, Transform whereFrom)
     {
         //raycasts forward to anything with rigidbody
-        Ray ray = new Ray(transform.position, transform.forward); //checks negative up for down
+        Ray ray = new Ray(whereFrom.position, whereFrom.forward); //checks negative up for down
         RaycastHit hitData;
         if (Physics.Raycast(ray, out hitData, distanceToCheck))
         {
             if (hitData.rigidbody != null) //does what I hit have a rigidbody?
             {
-                Debug.Log(hitData.point); //returns exact world position of ray endpoint when we raycast
-
+                //Debug.Log(hitData.point); //returns exact world position of ray endpoint when we raycast
+                rigidHit = hitData.point;
+            }
+            else
+            {
+                rigidHit = Vector3.zero;
             }
 
         }
