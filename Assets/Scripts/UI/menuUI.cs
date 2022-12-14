58961a1c07c86862303c2bf05dd72f31ef3d75a2
May <shanglan.may.li@gmail.com>
2022-12-14 06:29:40 -0500

    started UI stuff

---
+++ Assets/Scripts/UI/menuUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuUI : MonoBehaviour
+{
+
+    public Canvas menuCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
